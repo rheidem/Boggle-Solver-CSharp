bb788090d472b71b6bacf2dc2c84919cbc2f540b
rheidem <rheidem@umich.edu>
2020-08-10 22:55:11 -0400

    added Word class

---
+++ BoggleSolverCSharp/Word.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace BoggleSolverCSharp
+{
+    public class Word
+    {
+        public string text { get; set; }
+        public int score { get; set; }
+
+        public Word(string text_in)
+        {
+            text = text_in;
+
+            // Assign a score to the word based on its length
+            int len = text.Length;
+
+            if (len < 5)
+            {
+                score = 1;
+            }
+            else if (len == 5)
+            {
+                score = 2;
+            }
+            else if (len == 6)
+            {
+                score = 3;
+            }
+            else if (len == 7)
+            {
+                score = 5;
+            }
+            else
+            {
+                score = 11;
+            }
+        }
+    }
+}
